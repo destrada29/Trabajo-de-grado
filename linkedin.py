1f23ee6e184ceb24136b86fdc204ecdb173569c2
cscc5 <95cscc@gmail.com>
2023-09-10 19:37:59 -0500

    Analisis del WebScraping

--- linkedin.py
+++ linkedin.py
@@ -1,29 +1,35 @@
-from obb import Data_extract
 import time
-from pprint import pprint
+import datetime
+from obb import Data_extract
 
+hora = str(datetime.datetime.now())
 
-linkedin=Data_extract('https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0')
+linkedin = Data_extract('https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0')
 linkedin.ingresar_link()
 time.sleep(2)
 linkedin.busqueda_id("job-search-bar-keywords").send_keys('Informatica')
 time.sleep(2)
 linkedin.busqueda_id("job-search-bar-location").clear()
 time.sleep(2)
-linkedin.busqueda_id("job-search-bar-location").send_keys('Perímetro Urbano Medellín, Antioquia, Colombia')
-
+linkedin.busqueda_id("job-search-bar-location").send_keys('Colombia')
 time.sleep(2)
 linkedin.busqueda_xpath("//*[@id='jobs-search-panel']/form/button").click()
 time.sleep(2)
 linkedin.busqueda_xpath("//*[@id='jserp-filters']/ul/li[6]/div/div/button").click()
 time.sleep(2)
+"""
+En caso de tener ventana emergentes
 linkedin.busqueda_id('f_E-1').click()
 time.sleep(2)
 linkedin.busqueda_id('f_E-3').click()
-
 time.sleep(2)
+"""
 linkedin.busqueda_xpath("//*[@id='jserp-filters']/ul/li[6]/div/div/div/button").click()
 time.sleep(2)
 linkedin.scroll_down(10,'/html')
-linkedin.Obtener_perfiles('sr-only')
-pprint(linkedin.text_list)
+time.sleep(2)
+linkedin.Obtener_perfiles('base-search-card__title','base-search-card__subtitle','job-search-card__location')
+time.sleep(2)
+linkedin.Cerrar_drive()
+time.sleep(2)
+linkedin.Guardar_df(f'Trabajo-de-grado-ll/Resultados/Resultado_linkedin_{hora}.csv')
--- obb.py
+++ obb.py
@@ -1,24 +1,25 @@
-from selenium.webdriver.chrome.service import Service
+import time
+import platform
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver.common.keys import Keys
-import matplotlib.pyplot as plt
-import re
-import numpy as np
-
-
+from selenium.webdriver.chrome.service import Service
 
 class Data_extract():
+    
     def __init__(self,link):
         self.link= link
-        self.servicio= Service(executable_path="trabajo_selenium\chromedriver.exe")
+        if platform.system() == "Windows":
+            self.servicio = Service(executable_path = "trabajo_selenium\chromedriver.exe")
+        else:
+            self.servicio = Service(executable_path = "/usr/local/bin/chromedriver")
+
         self.driver = webdriver.Chrome(service=self.servicio)
     
     def ingresar_link(self):
         return self.driver.get(self.link)
-        
-
+    
     def busqueda_xpath(self,dato):
         return self.driver.find_element(By.XPATH,dato)
     
@@ -30,29 +31,21 @@ class Data_extract():
             self.driver.find_element(By.XPATH,dato).send_keys(Keys.END)
             time.sleep(2)
 
-    def Obtener_perfiles(self,dato):
-        self.text_list = [element.text for element in self.driver.find_elements(By.CLASS_NAME,dato)]
+    def Obtener_perfiles(self,title,company,location):
+        self.title = [element.text for element in self.driver.find_elements(By.CLASS_NAME,title)]
+        self.location = [element.text for element in self.driver.find_elements(By.CLASS_NAME,location)]
+        self.company = [element.text for element in self.driver.find_elements(By.CLASS_NAME,company)]
 
+    def Guardar_df(self,name):
+        df_jobs = pd.DataFrame({'Title' : self.title, 'Location' : self.location, 'Company' : self.company})
+        df_jobs.to_csv(name, index=False)
+    
     def Cerrar_drive(self):
         self.driver.close()
     
     def obtener_perfiles_paginados(self,dato,num,xpath):
-        self.text_list = [[element.text for element in self.driver.find_elements(By.CLASS_NAME,dato)]]
+        self.title = [[element.text for element in self.driver.find_elements(By.CLASS_NAME,dato)]]
         for i in range(num):
             self.busqueda_xpath(xpath).click()
             time.sleep(4)
-            self.text_list.append([element.text for element in self.driver.find_elements(By.CLASS_NAME,dato)])
-
-
-         
-
-
-
-
-
-
-
-
-
-
-
+            self.title.append([element.text for element in self.driver.find_elements(By.CLASS_NAME,dato)])
