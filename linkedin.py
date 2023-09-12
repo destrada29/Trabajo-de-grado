a530fb612ac591f7172b0874b2c7893f7ba7cea8
cscc5 <95cscc@gmail.com>
2023-09-12 10:52:55 -0500

    Se adicionan las categorias

--- linkedin.py
+++ linkedin.py
@@ -15,21 +15,26 @@ linkedin.busqueda_id("job-search-bar-location").send_keys('Colombia')
 time.sleep(2)
 linkedin.busqueda_xpath("//*[@id='jobs-search-panel']/form/button").click()
 time.sleep(2)
+
+"""
+filtro de modalidad
 linkedin.busqueda_xpath("//*[@id='jserp-filters']/ul/li[6]/div/div/button").click()
 time.sleep(2)
-"""
-En caso de tener ventana emergentes
+
+#En caso de tener ventana emergentes:
 linkedin.busqueda_id('f_E-1').click()
 time.sleep(2)
 linkedin.busqueda_id('f_E-3').click()
 time.sleep(2)
-"""
+
 linkedin.busqueda_xpath("//*[@id='jserp-filters']/ul/li[6]/div/div/div/button").click()
 time.sleep(2)
+"""
+
 linkedin.scroll_down(10,'/html')
 time.sleep(2)
 linkedin.Obtener_perfiles('base-search-card__title','base-search-card__subtitle','job-search-card__location')
 time.sleep(2)
 linkedin.Cerrar_drive()
 time.sleep(2)
-linkedin.Guardar_df(f'Trabajo-de-grado-ll/Resultados/Resultado_linkedin_{hora}.csv')
+linkedin.Guardar_df(f'Trabajo-de-grado/Resultados/Resultado_{hora}.csv')
